3a9fb50307638b139fed713df3661d4141b462de
ccarvalhares <kadu_carvalhares@yahoo.com.br>
2025-11-25 00:37:11 -0300

    Fix Express 5 wildcard route crash

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.use(express.static(path.join(__dirname, 'client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
     });
 } else {
